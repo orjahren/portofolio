02109939f80c7136535018058b596cbb87fa4ea8
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-07-18 14:24:54 +0200

    Ha en head, set meta tags for MaterialUI og charset.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,11 @@ export const metadata: Metadata = {
 
 export default ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
+    <head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <meta charSet="UTF-8" />
+    </head>
+
     <body className={inter.className}>{children}</body>
   </html>
 );
