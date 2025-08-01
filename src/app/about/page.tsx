31c6f9b800adb13b8962604da480b4d680d96652
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-08-01 10:52:49 +0200

    Routes for about og contact.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>TODO: Skrive about</p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+const ContactPage = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>TODO: Skrive contact</p>
+    </div>
+  );
+};
+
+export default ContactPage;
