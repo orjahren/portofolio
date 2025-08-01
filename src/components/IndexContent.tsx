caa03fd92dbfe2ac79472c643c76a6df0b0a0a7d
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-08-01 11:29:13 +0200

    Bruk MUI for index content.

--- src/components/IndexContent.tsx
+++ src/components/IndexContent.tsx
@@ -1,81 +1,125 @@
-import style from "./index.module.scss";
-import ProfilePicture from "/public/oliver_jahren.png";
+import {
+  Box,
+  Typography,
+  Link,
+  Grid,
+  Avatar,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Paper,
+} from "@mui/material";
 
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import LanguageIcon from "@mui/icons-material/Language";
 import Image from "next/image";
+import ProfilePicture from "/public/oliver_jahren.png";
+import { JSX } from "react";
 
-const IndexContent = () => {
-  const linkPairs: ReadonlyArray<[string, string]> = [
-    ["Github", "https://github.com/orjahren"],
-    ["LinkedIn", "https://www.linkedin.com/in/orjahren"],
-    ["Kattis", "https://open.kattis.com/users/oliverrj"],
-  ];
+const linkPairs: ReadonlyArray<[string, string, JSX.Element]> = [
+  ["Github", "https://github.com/orjahren", <GitHubIcon />],
+  ["LinkedIn", "https://www.linkedin.com/in/orjahren", <LinkedInIcon />],
+  ["Kattis", "https://open.kattis.com/users/oliverrj", <LanguageIcon />],
+];
 
-  return (
-    <div className={style.mainContentWrapper}>
-      <div className={style.contentDiv}>
-        <div>
-          <div className={style.textDiv}>
-            <div>
-              <h1>Oliver Jahren</h1>
-              <p className={style.email}>oliverrj@ifi.uio.no</p>
-            </div>
-            <div className={style.contentText}>
-              <p>
-                I am studiyng for a{" "}
-                <a href="https://www.uio.no/english/studies/programmes/informatics-programming-master/index.html">
-                  masters degree in computer science at the University of Oslo
-                </a>
-                , previously having completed a bachelor's degree in{" "}
-                <a href="https://www.uio.no/studier/program/informatikk-sprakteknologi/">
-                  natural language processing and artificial intelligence
-                </a>{" "}
-                at the same university.
-              </p>
-              <p>
-                I work as a web developer on the research service{" "}
-                <a href="https://nettskjema.no/?lang=en">Nettskjema.no</a>, and
-                as a teaching assistant in{" "}
-                <a href="https://www.uio.no/studier/emner/matnat/ifi/IN4120/index-eng.html">
-                  search technology
-                </a>
-                .
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={style.photoAndLinks}>
-          <div>
-            <div className={style.personalDiv}>
-              <div>
-                <div className={style.profilePictureDiv}>
-                  <Image
-                    src={ProfilePicture}
-                    alt="Picture of the author"
-                    width={300}
-                    placeholder="blur"
-                  />
-                  <p style={{ fontSize: "1rem", marginBottom: 10 }}>
-                    <i>All rights reserved</i>
-                  </p>
-                </div>
-              </div>
-              <div>
-                <div className={style.linkDiv}>
-                  <ul>
-                    {linkPairs.map(([name, url]) => (
-                      <li key={url}>
-                        <a href={url}>{name}</a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+const IndexContent = () => (
+  <Box sx={{ py: 4, px: { xs: 2, md: 6 } }}>
+    <Paper
+      elevation={3}
+      sx={{ p: { xs: 2, md: 4 }, maxWidth: 900, mx: "auto" }}
+    >
+      <Grid container spacing={4} alignItems="center">
+        <Grid>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Oliver Jahren
+          </Typography>
+          <Typography variant="body1" color="text.secondary" sx={{ mb: 2 }}>
+            <AccountCircleIcon fontSize="small" sx={{ mr: 1 }} />
+            oliverrj@ifi.uio.no
+          </Typography>
+          <Typography variant="body1" sx={{ mb: 2 }}>
+            I am studying for a{" "}
+            <Link
+              href="https://www.uio.no/english/studies/programmes/informatics-programming-master/index.html"
+              target="_blank"
+              rel="noopener"
+            >
+              masters degree in computer science at the University of Oslo
+            </Link>
+            , previously having completed a bachelor's degree in{" "}
+            <Link
+              href="https://www.uio.no/studier/program/informatikk-sprakteknologi/"
+              target="_blank"
+              rel="noopener"
+            >
+              natural language processing and artificial intelligence
+            </Link>{" "}
+            at the same university.
+          </Typography>
+          <Typography variant="body1">
+            I work as a web developer on the research service{" "}
+            <Link
+              href="https://nettskjema.no/?lang=en"
+              target="_blank"
+              rel="noopener"
+            >
+              Nettskjema.no
+            </Link>
+            , and as a teaching assistant in{" "}
+            <Link
+              href="https://www.uio.no/studier/emner/matnat/ifi/IN4120/index-eng.html"
+              target="_blank"
+              rel="noopener"
+            >
+              search technology
+            </Link>
+            .
+          </Typography>
+        </Grid>
+        <Grid
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            sx={{ width: 180, height: 180, mb: 2 }}
+            alt="Picture of the author"
+          >
+            <Image
+              src={ProfilePicture}
+              alt="Picture of the author"
+              width={180}
+              height={180}
+              style={{ borderRadius: "50%" }}
+            />
+          </Avatar>
+          <Typography variant="caption" sx={{ mb: 2 }}>
+            <i>All rights reserved</i>
+          </Typography>
+          <List>
+            {linkPairs.map(([name, url, icon]) => (
+              <ListItem key={url} disablePadding>
+                <ListItemIcon sx={{ minWidth: 36 }}>{icon}</ListItemIcon>
+                <Link
+                  href={url}
+                  target="_blank"
+                  rel="noopener"
+                  underline="hover"
+                >
+                  <ListItemText primary={name} />
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+      </Grid>
+    </Paper>
+  </Box>
+);
 
 export default IndexContent;
