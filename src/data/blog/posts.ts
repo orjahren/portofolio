9c2b11be69d18181d7b9b534b1c87968b64eaadc
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-08-01 13:27:21 +0200

    Blogpost om falske løfter.

--- src/data/blog/posts.ts
+++ src/data/blog/posts.ts
@@ -21,6 +21,17 @@ const posts: ReadonlyArray<PostType> = [
       "Noen tanker om moralens grunnleggende betydning og hvordan den påvirker våre handlinger og hvorfor den finnes.",
     link: "/blog/hva-er-poenget-med-moralen",
   },
+  {
+    title: "Kan det være riktig å avgi falske løfter?",
+    date: new Date("2025-05-30"),
+    keyword: "falske-lofter",
+    fileName: "kan-det-være-riktig-a-avgi-falske-lofter.mdx",
+    blurb:
+      "Hvorfor har vi løfter, og kan det noen gang være riktig å avgi falske løfter?",
+    description:
+      "Noen tanker om hvorfor man har løfter og om det kan være riktig å avgi falske løfter.",
+    link: "/blog/falske-lofter",
+  },
 ];
 
 export default posts;
