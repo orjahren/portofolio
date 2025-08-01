bd54b1f281cadc823b5d007fcc73e8939dedd28b
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-08-01 10:09:06 +0200

    Navigation i footer.

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -2,10 +2,19 @@ import style from "./footer.module.scss";
 
 const Footer = ({ githubRepoUrl }: { githubRepoUrl: string }) => (
   <footer className={style.footer}>
-    <p>
-      🚀 Made with coffee in Norway, 2023.{" "}
-      <a href={githubRepoUrl}>Source code on Github</a> 🦄
-    </p>
+    <div>
+      <p>
+        <a href="/">Home</a> | <a href="/projects">Projects</a> |{" "}
+        <a href="/about">About</a> | <a href="/contact">Contact</a>
+      </p>
+    </div>
+    <div>
+      <p>
+        🚀 Made with coffee in Norway, 2023-25.{" "}
+        <a href={githubRepoUrl}>Source code on Github</a>. 🦮
+      </p>
+      <p>0372 Oslo.</p>
+    </div>
   </footer>
 );
 
